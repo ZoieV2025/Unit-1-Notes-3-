1ac646da2ce4dbabe6abc719c225d47b3c4f4fa7
ZoieV2025 <zoie.vitale2025@gbwl.org>
2024-09-09 09:35:18 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+//This is the basic structure of a Java class
+//Start a Java program by declaring the class
+//Class name MUST MATCH the file name (Main.java)
+
+public class Main{
+
+  //This is the MAIN METHOD (like a function)
+public static void main(String []largs) {
+  System.out.println("Hello World")
+    }
+}
